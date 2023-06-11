99a2493aba18571f54b1a0400af2f47a233a5bf5
Syncxv <aruldevarackal1@gmail.com>
2023-06-12 01:55:22 +1000

    smol optimization

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-#[macro_use]
 extern crate warp;
 use core::fmt;
 use serde::{Deserialize, Serialize};
-use std::{fs, rc::Rc};
+use std::{fs, rc::Rc, sync::Arc, time::Instant};
 use trie_rs::{Trie, TrieBuilder};
 use warp::Filter;
 
@@ -44,24 +43,28 @@ struct Request {
 
 #[tokio::main]
 async fn main() {
+    let words = read_lines("wordlist.txt");
+    let mut builder = TrieBuilder::new();
+    for word in &words {
+        builder.push(word);
+    }
+    let word_list = Arc::new(builder.build());
     // POST /words
     let words = warp::path("words")
         .and(warp::post())
         .and(warp::body::json())
-        .map(|req: Request| {
+        .and(with_word_list(word_list))
+        .map(|req: Request, word_list: Arc<Trie<u8>>| {
+            let start_time = Instant::now();
             let grid = generate_grid(&req.combo);
-            let words = read_lines("wordlist.txt");
-            let mut builder = TrieBuilder::new();
-            for word in &words {
-                builder.push(word);
-            }
-            let word_list = builder.build();
             let mut result: Vec<Vec<Vec<&Letter>>> = vec![];
 
             for i in 2..req.iterations + 1 {
                 result.push(get_combos(&grid, &word_list, i))
             }
 
+            let duration = start_time.elapsed();
+            println!("Time elapsed is: {:?}", duration);
             warp::reply::json(&result)
         });
 
@@ -196,4 +199,10 @@ fn read_lines(filename: &str) -> Vec<String> {
     }
 
     result
+}
+
+fn with_word_list(
+    word_list: Arc<Trie<u8>>,
+) -> impl Filter<Extract = (Arc<Trie<u8>>,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || word_list.clone())
 }
